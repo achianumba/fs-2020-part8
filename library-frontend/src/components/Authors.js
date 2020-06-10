a2d1f38b885a63e793da9c9d68bda7adf05cabe0
achianumba <chianumbaarinze@gmail.com>
2020-06-10 21:34:15 +0100

    Exercise 8.8: Added users data to authors view

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,38 +1,41 @@
-  
-import React from 'react'
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { ALL_AUTHORS } from "../queries";
 
 const Authors = (props) => {
+  const authors = useQuery(ALL_AUTHORS);
+
   if (!props.show) {
-    return null
+    return null;
   }
-  const authors = []
+
+  if (authors.loading) {
+    return <h2>Loading users...</h2>
+  }
 
   return (
     <div>
       <h2>authors</h2>
       <table>
-        <tbody>
+        <thead>
           <tr>
-            <th></th>
-            <th>
-              born
-            </th>
-            <th>
-              books
-            </th>
+            <th>born</th>
+            <th>books</th>
           </tr>
-          {authors.map(a =>
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
-            </tr>
-          )}
+        </thead>
+        <tbody>
+          {authors &&
+            authors.data.allAuthors.map((a) => (
+              <tr key={a.name}>
+                <td>{a.name}</td>
+                <td>{a.born}</td>
+                <td>{a.bookCount}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
-
     </div>
-  )
-}
+  );
+};
 
-export default Authors
+export default Authors;
