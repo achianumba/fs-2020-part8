fa8e7e0b057b5002ca1fdf51f1552735a4dd4fe8
achianumba <chianumbaarinze@gmail.com>
2020-06-20 21:11:01 +0100

    Create bookSchema and Book model

---
+++ library-backend/models/books.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const bookSchema = new Schema({
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 2
+    },
+    published: {
+      type: Number,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Author'
+    },
+    genres: [
+      { type: String}
+    ]
+  });
+
+  const Book = model('Book', bookSchema);
