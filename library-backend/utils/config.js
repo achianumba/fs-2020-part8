312c4c377bdb18c9466e9e54c5d1aabc4746e3ec
achianumba <chianumbaarinze@gmail.com>
2020-06-20 23:10:41 +0100

    Add connectToMongoDB fn

---
+++ library-backend/utils/config.js
@@ -0,0 +1,28 @@
+const { env } = process;
+const { connect } = require('mongoose');
+
+if (env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const MONGO_URI = env.NODE_ENV === 'production' ?
+env.MONGO_URI : env.MONGO_URI_DEV;
+
+const connectToMongoDB = () => {
+    connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
+    .then(() => {
+        console.log('Connected to MongoDB successfully');
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
+
+module.exports = {
+    connectToMongoDB
+};
