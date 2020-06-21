0c5e213b6b69b27e7b7895c0fc31cf7270a177ab
achianumba <chianumbaarinze@gmail.com>
2020-06-21 12:14:48 +0100

    Add context to server

--- library-backend/index.js
+++ library-backend/index.js
@@ -1,13 +1,26 @@
 const { ApolloServer } = require("apollo-server");
+const { verify } = require('jsonwebtoken');
 const resolvers = require('./resolvers');
 const typeDefs = require('./types');
-const { connectToMongoDB } = require('./utils/config')
+const { connectToMongoDB, JWT_SECRET } = require('./utils/config')
+const { getUserByUserId } = require('./models/users');
 
 connectToMongoDB();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null;
+
+    if (auth && auth.toLocaleLowerCase().startsWith('bearer ')) {
+      const token = await verify(auth.slice(7), JWT_SECRET);
+      const user = await getUserByUserId(token.id);
+      return {
+        user: user.toJSON()
+      }
+    }
+  }
 });
 
 server.listen().then(({ url }) => {
