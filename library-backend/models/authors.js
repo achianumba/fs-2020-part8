d63e77faa0a48ebc471ec6fd246887e2f833c953
achianumba <chianumbaarinze@gmail.com>
2020-06-21 01:54:17 +0100

    Set up and export fn for CRUD operations: {   getAuthors,   getAuthorByName,   countAuthors,   createAuthor,   editAuthorBirthYear }

--- library-backend/models/authors.js
+++ library-backend/models/authors.js
@@ -12,4 +12,34 @@ const authorSchema = new Schema({
   },
 });
 
+authorSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
 const Author = model('Author', authorSchema);
+
+const getAuthors = () => Author.find();
+const getAuthorByName = (name) => Author.findOne({ name: name });
+
+const editAuthorBirthYear = (name, born) => {
+  return Author.findOneAndUpdate(
+    { name },
+    { $set: { born }},
+    { new: true }
+    );
+}
+
+const countAuthors = () => Author.countDocuments({});
+const createAuthor = (author) => new Author(author).save();
+
+module.exports = {
+  getAuthors,
+  getAuthorByName,
+  countAuthors,
+  createAuthor,
+  editAuthorBirthYear
+}
