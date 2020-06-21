a516f5838b667bcef603c2e2449be577a1ab12d1
achianumba <chianumbaarinze@gmail.com>
2020-06-21 02:05:23 +0100

    connectToMongoDB

--- library-backend/index.js
+++ library-backend/index.js
@@ -3,7 +3,7 @@ const resolvers = require('./resolvers');
 const typeDefs = require('./types');
 const { connectToMongoDB } = require('./utils/config')
 
-connectToMongoDB()
+connectToMongoDB();
 
 const server = new ApolloServer({
   typeDefs,
