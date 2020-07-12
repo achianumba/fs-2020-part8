8846057e5d95605bec9a571f15099fb0e1c28523
achianumba <chianumbaarinze@gmail.com>
2020-07-12 12:09:06 +0100

    8.21: Move queries for fetching books into child components - Remove loading and data props from Books and Recommended components

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Authors from './components/Authors';
 import Books from './components/Books';
 import NewBook from './components/NewBook';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Recommended from './components/Recommended';
-import { useQuery } from '@apollo/client';
-import { ALL_BOOKS } from './queries';
 
 const App = () => {
   const [page, setPage] = useState('authors');
   const [token, setToken] = useState(null);
-  const booksRequest = useQuery(ALL_BOOKS);
+
+  useEffect(() => {
+    if (localStorage.token) {
+      setToken(localStorage.token);
+    }
+  }, []);
 
   const logout = e => {
     setToken(null);
@@ -37,14 +40,10 @@ const App = () => {
 
       <Books
         show={page === 'books'}
-        loading={booksRequest.loading}
-        data={booksRequest.data}
       />
 
       <Recommended
         show={page === 'recommended'}
-        loading={booksRequest.loading}
-        data={booksRequest.data}
       />
 
       <NewBook
